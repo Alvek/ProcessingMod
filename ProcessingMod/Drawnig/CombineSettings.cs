e537441f07c083c7666f24660347f27ca9f3e99e
Alvek <cj9ddrd@gmail.com>
2017-11-23 23:29:32 +0200

    Combine class stub

---
+++ ProcessingMod/Drawnig/CombineSettings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCE.UTscanner.Processing.Drawnig
+{
+    public class CombineSettings
+    {
+        public List<CombineRule> Rules { get; }
+    }
+
+    public class CombineRule
+    {
+        /// <summary>
+        /// Битовая маска гейтов которые нужно объединять
+        /// </summary>
+        public int GateCombineMask { get { return _gateCombineMask; } }
+        /// <summary>
+        /// Список каналов которые объединяются
+        /// </summary>
+        public List<int> ChannelsIds { get { return _channelsIds; } }
+
+        private List<int> _channelsIds;
+        private int _gateCombineMask;
+    }
+}
