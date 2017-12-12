4900d482b5106d1096ef4e573980efe90b4ff8e3
Alvek <cj9ddrd@gmail.com>
2017-12-12 22:56:47 +0200

    Summ mod, removed obsolete params in drawer const.

--- ProcessingMod/Drawnig/CombineSettings.cs
+++ ProcessingMod/Drawnig/CombineSettings.cs
@@ -23,7 +23,7 @@ namespace NCE.UTscanner.Processing.Drawnig
         private List<int> _channelsIds;
         private int _gateCombineMask;
         private int _gatesCount;
-
+        private int _displayId;
         /// <summary>
         /// Битовая маска гейтов которые нужно объединять
         /// </summary>
@@ -34,12 +34,13 @@ namespace NCE.UTscanner.Processing.Drawnig
         public List<int> ChannelsIds { get { return _channelsIds; } }
 
         public int GatesCount { get { return _gatesCount; } }
+        public int DisplayId { get { return _displayId; } }
 
-        public CombineRule(List<int> combineChannelsIds, int gateMask)
+        public CombineRule(List<int> combineChannelsIds, int gateMask, int displayId)
         {
             _channelsIds = combineChannelsIds;
             _gateCombineMask = gateMask;
-
+            _displayId = displayId;
             _gatesCount = gateMask / 2 + gateMask % 2;
         }
     }
