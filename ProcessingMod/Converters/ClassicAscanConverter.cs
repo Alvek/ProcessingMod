12410d5e83b27f2177835c11d3ae379c72ed527d
Oessau <oessau@gmail.com>
2017-12-27 16:01:31 +0200

    /100

--- ProcessingMod/Converters/ClassicAscanConverter.cs
+++ ProcessingMod/Converters/ClassicAscanConverter.cs
@@ -177,7 +177,7 @@ namespace NCE.UTscanner.Processing.Drawing
             int endTime = manager.GetAscanEndTime(rawArray, coordinateOffset);
             foreach (var point in manager.AscanIterator(rawArray, coordinateOffset))
             {
-                double x = (startTime + (double)(idx * (endTime - startTime)) / scansMaxCount) / 1000;
+                double x = (startTime + (double)(idx * (endTime - startTime)) / scansMaxCount) / 100;
                 res.Add(new PointPair(x, point));
                 idx++;
             }
